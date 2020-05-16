0cab4d4cd051393a45ae7ce5307dbdaf3f536525
April Howard <ahoward@zipcodes-MBP-5.hitronhub.home>
2020-05-16 05:07:58 -0400

    xml config

--- MyApp.java
+++ MyApp.java
@@ -1,14 +1,14 @@
-package com.cameronscaptures;
+package com.luv2code.springdemo;
 
 public class MyApp {
 
 	public static void main(String[] args) {
 
-		//create the object
+		// create the object
 		Coach theCoach = new TrackCoach();
-			
+		
 		// use the object
-		System.out.println(theCoach.getDailyWorkout());
+		System.out.println(theCoach.getDailyWorkout());		
 	}
 
 }
